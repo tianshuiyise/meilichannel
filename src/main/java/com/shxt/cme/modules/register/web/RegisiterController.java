4b60cab0a335e3af91668b46458a8632ac989b57
yu280265067 <yuchengxiang.ads@163.com>
2015-07-25 16:53:38 +0800

    短信

--- src/main/java/com/shxt/cme/modules/register/web/RegisiterController.java
+++ src/main/java/com/shxt/cme/modules/register/web/RegisiterController.java
@@ -1,7 +1,16 @@
 package com.shxt.cme.modules.register.web;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cloopen.rest.sdk.CCPRestSmsSDK;
 
 /** 
  * @Project:  美丽频道    
@@ -17,9 +26,113 @@ public class RegisiterController {
 	
 	@RequestMapping(value = "preRegisterByPhone")
 	public String registerByPhone(){
-		
-		
+		System.out.println("短信验证ajax被调用");
 		return "/register/registerByPhone";
 	}
+	@RequestMapping(value = "msgGet")
+	@ResponseBody
+	public String msgGet(String phoneNum,HttpSession session){
+		Map<String, Object> modelMap =msgCheckUtils(phoneNum,session);
+		if(session.getAttribute(phoneNum)==null||session.getAttribute(phoneNum)==""){
+			return "号码不正确";
+		}
+		else{
+		int checkNum= (Integer) session.getAttribute(phoneNum);//需健壮性判断
+		String checkString=String.valueOf(checkNum);
+		System.out.println("验证码为=="+checkNum);
+		System.out.println(modelMap.get("statusMsg")+"短信验证ajax被调用");
+		return checkString;
+		}
+	}
+	@RequestMapping(value = "msgCheck")
+	@ResponseBody
+	public String msgCheck(String phoneNum,HttpSession session,String checkNum){
+		System.out.println("msgCheck方法中checkNum=="+session.getAttribute(phoneNum));
+		System.out.println("从前台传过来的checkNum=="+checkNum);
+		if(checkNum==null||checkNum==""){
+			System.out.println("进入空校验");
+			session.setAttribute("checkedInfo", "1");
+			return "验证码不能为空";
+		}
+		else if(session.getAttribute(phoneNum)==null||session.getAttribute(phoneNum)==""){
+			session.setAttribute("checkedInfo", "1");
+			System.out.println("进入空校验");
+			return "验证码错误";
+		}
+		else if(session.getAttribute(phoneNum).toString().equals(checkNum)){
+			session.setAttribute("checkedInfo", "2");
+			return "验证码正确";
+		}else{
+			session.setAttribute("checkedInfo", "1");
+			return "验证码错误，请重试";
+		}
+		
+	}
+	public HashMap<String, Object>  msgCheckUtils(String phoneNum,
+			HttpSession session){
 
+		HashMap<String, Object> result = null;
+
+		//初始化SDK
+		CCPRestSmsSDK restAPI = new CCPRestSmsSDK();
+		
+		//******************************注释*********************************************
+		//*初始化服务器地址和端口                                                       *
+		//*沙盒环境（用于应用开发调试）：restAPI.init("sandboxapp.cloopen.com", "8883");*
+		//*生产环境（用户应用上线使用）：restAPI.init("app.cloopen.com", "8883");       *
+		//*******************************************************************************
+		restAPI.init("sandboxapp.cloopen.com", "8883");
+		
+		//******************************注释*********************************************
+		//*初始化主帐号和主帐号令牌,对应官网开发者主账号下的ACCOUNT SID和AUTH TOKEN     *
+		//*ACOUNT SID和AUTH TOKEN在登陆官网后，在“应用-管理控制台”中查看开发者主账号获取*
+		//*参数顺序：第一个参数是ACOUNT SID，第二个参数是AUTH TOKEN。                   *
+		//*******************************************************************************
+		restAPI.setAccount("8a48b5514d232afc014d2738ab5b02ae", "e0c7b2437c5c41a2bd88ed7ace503771");
+		
+		
+		//******************************注释*********************************************
+		//*初始化应用ID                                                                 *
+		//*测试开发可使用“测试Demo”的APP ID，正式上线需要使用自己创建的应用的App ID     *
+		//*应用ID的获取：登陆官网，在“应用-应用列表”，点击应用名称，看应用详情获取APP ID*
+		//*******************************************************************************
+		restAPI.setAppId("8a48b5514d232afc014d27396f0302b2");
+		
+		
+		//******************************注释****************************************************************
+		//*调用发送模板短信的接口发送短信                                                                  *
+		//*参数顺序说明：                                                                                  *
+		//*第一个参数:是要发送的手机号码，可以用逗号分隔，一次最多支持100个手机号                          *
+		//*第二个参数:是模板ID，在平台上创建的短信模板的ID值；测试的时候可以使用系统的默认模板，id为1。    *
+		//*系统默认模板的内容为“【云通讯】您使用的是云通讯短信模板，您的验证码是{1}，请于{2}分钟内正确输入”*
+		//*第三个参数是要替换的内容数组。																														       *
+		//**************************************************************************************************
+		
+		//**************************************举例说明***********************************************************************
+		//*假设您用测试Demo的APP ID，则需使用默认模板ID 1，发送手机号是13800000000，传入参数为6532和5，则调用方式为           *
+		//*result = restAPI.sendTemplateSMS("13800000000","1" ,new String[]{"6532","5"});																		  *
+		//*则13800000000手机号收到的短信内容是：【云通讯】您使用的是云通讯短信模板，您的验证码是6532，请于5分钟内正确输入     *
+		//*********************************************************************************************************************
+		int checkNum=(int) (Math.random()*9000+1000);
+		String[] checkMsg=new String[]{""+checkNum,"5"};
+		
+		result = restAPI.sendTemplateSMS(phoneNum,"1" ,checkMsg);
+		System.out.println("SDKTestGetSubAccounts result=" + result);
+		if("000000".equals(result.get("statusCode"))){
+			session.setAttribute(phoneNum, checkNum);
+			System.out.println("statusCode=000000");
+			//正常返回输出data包体信息（map）
+			HashMap<String, Object> hashMap = (HashMap<String, Object>) result.get("data");
+			HashMap<String,Object> data = hashMap;
+			Set<String> keySet = data.keySet();
+			for(String key:keySet){
+				Object object = data.get(key);
+				System.out.println(key +" = "+object);
+			}
+		}else{
+			//异常返回输出错误码和错误信息
+			System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
+		}
+		return result;
+	}
 }
--- src/main/webapp/WEB-INF/views/register/registerByPhone.jsp
+++ src/main/webapp/WEB-INF/views/register/registerByPhone.jsp
@@ -19,49 +19,54 @@
 DD_belatedPNG.fix('#logo,.navBar,#top1,#top2,#top3,#top4,.nav_icon1,.nav_icon2,.nav_icon3,.nav_icon4,.shade,input,');
 </script>
 <![endif]-->
+<script>
+function msgCheck(){
+		var phoneNum = $("#phoneNum").val();
+		alert(phoneNum);
+		jQuery.ajax({
+			type : 'GET',     
+			contentType : 'application/json',     
+			url : '${ctx }/register/msgGet?phoneNum='+phoneNum,
+			//dataType : 'json',
+			success : function(data) {
+				alert(data);
+			}
+	    });
+	};
+function checkNumf(){
+	var phoneNum = $("#phoneNum").val();
+	var checkNum = $("#checkNum").val();
+	
+	jQuery.ajax({
+		type : 'GET',     
+		contentType : 'application/json',     
+		url : '${ctx }/register/msgCheck?phoneNum='+phoneNum+'&checkNum='+checkNum,
+		//dataType : 'json',
+		success : function(data) {
+			alert(data);
+		}
+    });
+}
+</script>
 </head>
 
 <body>
   <!--头部-->
-<div class="headerBar">
-  <div class="header comWidth">
-    <div class="fl"><img src="images/header/logo.png" alt="logo" id="logo" /></div>
-    <div class="rightArea fr">
-      <div class="topBar">
-        <ul>
-          <li id="top1"><a href="#">注册</a></li>
-          <li id="top2"><a href="#">登录</a></li>
-          <li id="top3"><a href="#">我的频道</a></li>
-          <li id="top4"><a href="#">申请商家入驻</a></li>
-        </ul>
-      </div>
-      <div class="navBar">
-        <ul>
-          <li class="nav_icon1"><a href="#">首页</a></li>
-          <li class="nav_icon2"><a href="#">美容</a></li>
-          <li class="nav_icon3"><a href="#">美发</a></li>
-          <li class="nav_icon4"><a href="#">美甲</a></li>
-        </ul>
-      </div>
-      <div id="search">
-        <input type="text" id="search_ipt" />
-        <input type="button" id="search_btn" />
-      </div>
-    </div>
-  </div>
-</div>
-<div id="headersd"></div>
-<!--头部end-->
+<c:import url="/WEB-INF/views/commonFile/head.jsp"></c:import>
 
   
 <form class="login">
-  <div id="mobile">*&nbsp;手机号码&nbsp;:<img src="${ctx}/static/images/+86.png"/><input type="text" /></div>
-  <div id="verify_code">&nbsp;&nbsp;&nbsp;*&nbsp;验证码&nbsp;:<input type="text" /></div>
-  <input id="get_code" type="button" />
+  <div id="mobile">*&nbsp;手机号码&nbsp;:<img src="${ctx}/static/images/+86.png"/><input id="phoneNum" type="text"  /></div>
+  <div id="verify_code">&nbsp;&nbsp;&nbsp;*&nbsp;验证码&nbsp;:
+ 
+  <input id="checkNum" type="text" onchange="checkNumf()"/>
+  </div>
+  <input id="get_code" type="button"  onclick="msgCheck()"/>
+ 
   <div id="remark">*&nbsp;为必填内容</div>
    <input id="agreement" name="agreement" type="radio" onclick="if(this.c==1){this.c=0;this.checked=0}else this.c=1" c="0" />
   <span id="agr"><label for="agreement">同意协议</label>&nbsp;/&nbsp;<a href="#">阅读协议</a></span>
-  <input id="next" type="submit" value="" />
+  <input id="next" type="submit" value="" onclick=""/>
   <a href="#"><span id="skp">邮箱注册</span>
   <img id="skip" src="${ctx}/static/images/skip.png"/></a>
 </form>
@@ -75,6 +80,7 @@ DD_belatedPNG.fix('#logo,.navBar,#top1,#top2,#top3,#top4,.nav_icon1,.nav_icon2,.
                 $(this).removeClass("i");
             })
         });
+   
 </script>
 </body>
 </html>
